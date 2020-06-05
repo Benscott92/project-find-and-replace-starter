d9309523337ceabb278c73887c1bfa8794790e5b
BenScott <benscott92@gmail.com>
2020-06-05 16:08:41 -0400

    search-works

--- main.js
+++ main.js
@@ -35,4 +35,34 @@ function getCellElements (currentRowElement) {
 // time-cost of ignoring the quality and readability of your code.
 //
 // You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
+//Add a click event listener to the replaceAllButton.
+
+
+
+
+
+
+// On click but outside of a loop, assign the values of the two input textboxes ( findInput and replaceInput), so that you are only retrieving these values once per click.
+// Write a loop which loops over the rowElements array (which is already provided in the starter code).
+// Inside this loop, use the getCellElements() function (already provided in the starter code) and assign the resulting array of cell elements to a variable.
+// Write a nested loop which loops over the array of cell elements.
+// For each cell element, check if a cell contains the user-provided search string. Use the string method includes().
+// If a cell does contain the user-provided search string, use innerHTML and the string method replace() to replace the user-provided search string with the user-provided replacement string.
+// In all your loops, avoid using low-meaning index variables like "i" and "j". you have read them, if you prefer.
+
+
+replaceAllButton.addEventListener('click', function(){
+    let search = findInput.value
+    let newText = replaceInput.value
+    for(let outerIndex = 0; outerIndex < rowElements.length; outerIndex++){
+        const searchGrid = getCellElements(rowElements[outerIndex])
+
+        for(let innerIndex = 0; innerIndex < searchGrid.length; innerIndex++){
+            let currentZone = searchGrid[innerIndex]
+            if(currentZone.innerHTML.includes(search)){
+                let replaceText = currentZone.innerHTML.replace(search, newText)
+                currentZone.innerHTML = replaceText
+            }
+        }
+    }
+})
